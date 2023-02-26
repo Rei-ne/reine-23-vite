437b4482d1f0197738f2ae15a2a202797c525dfa
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-02-26 23:00:37 +0100

    chore: img filter

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import aboutImg from '../assets/images/about-img.png'
 
 function About() {
     return (
-        <section id="about" className="h-fit md:h-fit about-div mt-10 flex flex-col justify-center items-center">
+        <section id="about" className="h-fit md:h-fit lg:h-full about-div mt-10 flex flex-col justify-center items-center">
             <h2 className="sm:text-2xl md:text-2xl uppercase flex justify-center items-center font-Bebas_Neue">About me</h2>
             <div className="max-h-1/4 w-4/5 text-gray-light md:flex flex-wrap md:flex-col lg:flex-row md:justify-around md:items-center md:h-4/5">
                 <div className="font-GT_Flexa p-6 md:w-fit lg:w-1/3 text-sm max-w-prose leading-8 left-about">
@@ -13,7 +13,7 @@ function About() {
                     <p className="my-2">I say that because right now I’m a frontend developer that aspires to be a fullstack blockchain engineer someday.</p>
                     <p className="my-2">My first dive into development was in August 2021, when I decided to transition from conventional application technical support, and take time out to give this new found interest all my attention. </p>
                 </div>
-                <div className="font-GT_Flexa grayscale p-6 flex flex-col justify-center items-center md:w-1/3">
+                <div className="font-GT_Flexa p-6 flex  flex-col justify-center items-center md:w-1/3">
                     <img className="bg-black grayscale text-gray h-48 w-48 object-cover md:h-full md:w-48 lg:w-64" src={aboutImg} alt="about reine dev" />
 
                 </div>
