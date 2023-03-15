f6a54d97b71feb75d32b56298af9acf0f5fa563e
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-03-15 08:16:23 +0100

    chore: contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import reine from '../assets/images/reine-img.png'
 import { useState } from 'react'
 
-import '../styles/Contact.scss'
-const Contact = () => {
+import '../styles/Contact.scss';
+
+function Contact() {
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -28,23 +29,23 @@ const Contact = () => {
     };
 
     return (
-        <section className="font-GT_Flexa  mt-10 mb-10 flex flex-row flex-wrap justify-center  items-center h-screen relative" id="contact">
-            <h2 className="font-Bebas_Neue sm:text-3xl md:text-3xl px-2 uppercase flex text-center justify-center items-center w-full ">GET IN TOUCH</h2>
-            <div className='sm:hidden md:flex items-center justify-center w-1/2 h-full'>
-                <img className="" src={reine} alt="a picture of reine" />
+        <section className="font-GT_Flexa  mt-10 mb-10 flex flex-col md:flex-row md:flex-wrap justify-center  items-center h-screen relative" id="contact">
+            <h2 className="font-Bebas_Neue sm:text-3xl md:text-3xl px-2 uppercase flex text-center justify-center items-center w-full">GET IN TOUCH</h2>
+            <div className='sm:hidden md:flex items-center justify-center w-1/2 h-auto'>
+                <img className="flex items-center justify-center" src={reine} alt="a picture of reine" />
             </div>
-            <div className='flex flex-col items-center justify-around lg:justify-center h-full md:w-1/2'>
-                <div className="flex flex-col items-center justify-center h-14  font-GT_Flexa w-4/5 p-6 md:w-full  text-sm max-w-prose text-left leading-8 text-gray-light">
-                    <div className='flex items-center h-fit md:w-fit md:h-auto justify-start text-left w-4/5'>
-                        <p className='max-w-prose sm:leading-6 leading-8 text-gray-light sm:text-base md:text-base' >Contact me if you’ve got remote job opportunities, web development gigs, collaborations or you just want to say hello 👋</p>
+            <div className='flex flex-col items-center justify-center md:justify-around h-auto w-4/5 md:w-1/2'>
+                <div className="flex flex-col items-center justify-center sm:h-10 h-12 font-GT_Flexa p-6 text-sm max-w-prose text-left leading-8 text-gray-light lg:max-w-md">
+                    <div className='flex items-center h-auto w-full justify-center text-left'>
+                        <p className='max-w-prose sm:leading-6 leading-8 text-gray-light sm:text-sm lg:text-base sm:text-center md:text-left' >Contact me if you’ve got remote job opportunities, web development gigs, collaborations or you just want to say hello 👋</p>
                     </div>
-                    <div className='flex items-center h-fit md:w-fit md:h-auto justify-start text-left w-4/5'>
-                        <p className='max-w-prose sm:hidden md:inline leading-8 text-gray-light sm:text-base md:text-base' >You can send me an <a className='text-bold underline' href="mailto:reinetoyosii@gmail.com">
+                    <div className='sm:hidden md:inline items-center h-fit w-full md:h-auto justify-start text-left'>
+                        <p className='max-w-prose sm:hidden md:inline leading-8 text-gray-light sm:text-base lg:text-base' >You can send me an <a className='text-bold underline' href="mailto:reinetoyosii@gmail.com">
                             email
                         </a> if contact forms aren't your thing</p>
                     </div>
                 </div>
-                <div className='mt-10 md:hidden'>
+                <div className='md:hidden'>
                     <button className='font-GT_Flexa uppercase border border-mint rounded-full w-10 h-5'>
                         <a className='no-underline' href="mailto:reinetoyosii@gmail.com">
                             send a mail
@@ -53,8 +54,8 @@ const Contact = () => {
                     </button>
                 </div>
                 {/* contact form */}
-                <div className='sm:hidden md:flex w-4/5 h-fit bg-gray-200'>
-                    <form className="font-GT_Flexa text-gray-light md:w-4/5  max-h-md flex flex-col bg-inherit max-w-md rounded shadow mx-auto" onSubmit={handleSubmit}>
+                <div className='sm:hidden md:flex max-w-md w-full h-fit bg-gray-200 mt-6'>
+                    <form className="font-GT_Flexa text-gray-light md:w-4/5  max-h-md flex flex-col bg-inherit rounded shadow mx-auto" onSubmit={handleSubmit}>
                         <div className="styled-input">
                             <input type="text" required value={name}
                                 onChange={e => setName(e.target.value)} />
@@ -87,8 +88,7 @@ const Contact = () => {
 
                 </div>
             </div>
-
-        </section >
+        </section>
     )
 }
 
