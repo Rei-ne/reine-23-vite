8089a6adb5ec9f8ca75f4829c71238bb07b6e8b6
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-03-17 14:55:18 +0100

    revert changes

--- functions/callback.js
+++
@@ -1,7 +0,0 @@
-// receives an auth code from spotify api
-exports.handler = (event, context, callback) => {
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(event.queryStringParameters),
-    });
-};
