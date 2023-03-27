706d19e09482dfea8a79fba33adef468aa14dd25
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-03-27 05:21:06 +0100

    fix: wa link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="bg-black flex flex-col lg:flex-row lg:justify-around lg:h-10 lg:border-t-2 lg:mt-10 border-gray w-full items-center justify-center uppercase h-14 p-8">
+        <div className="mt-6 bg-black flex flex-col lg:flex-row lg:justify-around lg:h-10 lg:border-t-2 lg:mt-10 border-gray w-full items-center justify-center uppercase h-14 p-8">
             <div>
                 <h3 className='h-6 flex items-center justify-center font-GT_Flexa text-2xl font-bold text-white text-center'>let's connect</h3>
                 <div className='font-GT_Flexa flex gap-4 text-sm md:text-1xl md:gap-8 items-center justify-center text-footer'>
-                    <a className='text-footer no-underline hover:text-mint hover:underline' target="_blank" href="https://wa.me/08174240017?text=">whatsapp</a>
+                    <a className='text-footer no-underline hover:text-mint hover:underline' target="_blank" href="https://wa.me/+2348174240017?text=">whatsapp</a>
                     <a className='text-footer no-underline hover:text-mint hover:underline' target="_blank" href="https://www.linkedin.com/in/toyosi-odukale/">linkedin</a>
                     <a className='text-footer no-underline hover:text-mint hover:underline' target="_blank" href="https://github.com/Rei-ne">github</a>
                 </div>
