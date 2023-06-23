24c7cb5cacf352d3b02bafce7c1b25ce203c60e5
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-06-23 15:50:04 +0100

    trying gtag.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import ReactGA from 'react-ga4'
 
-const token = import.meta.env.VITE_APP_GOOGLE_ANALYTICS
-ReactGA.initialize(`GA_MEASUREMENT_ID: ${token}`)
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
