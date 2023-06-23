b532c8bfbaf8ed283f5de696c42c3684f4fd28f6
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-06-23 12:43:13 +0100

    testing ga

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -33,7 +33,7 @@ const Home = () => {
     }
 
     useEffect(() => {
-        ReactGA.pageview(window.location.pathname)
+        ReactGA.pageview(window.location.pathname + window.location.search)
     }, [])
 
     return (
