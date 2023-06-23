6f03b68a0da123aa3422c9b1028b8c9da44bdce0
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-06-23 13:15:53 +0100

    -----

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ReactGA from 'react-ga4'
+
+const token = import.meta.env.VITE_APP_GOOGLE_ANALYTICS
+
+ReactGA.initialize(token)
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
