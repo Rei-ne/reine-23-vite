27101ab95199be337754e515381ba2b47cedbf26
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-06-23 15:54:45 +0100

    reversing gtag

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,11 @@ import Homepage from "./pages/Homepage"
 import './App.css';
 import Navbar from './components/Navbar';
 import Loader from './components/Loader';
+import ReactGA from 'react-ga4'
+
+const token = import.meta.env.VITE_APP_GOOGLE_ANALYTICS
+ReactGA.initialize(`GA_MEASUREMENT_ID: ${token}`)
+
 
 function App() {
 
