607d07df3e7c87afea0cdb9f157625a6da463ce5
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2024-02-06 08:07:09 +0000

    chore: updated work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -43,6 +43,15 @@ const projects = [
     },
     {
         id: 5,
+        title: "Method",
+        description: "A landing page for a payroll system built with react and tailwind CSS",
+        category: "javascript",
+        githubLink: null,
+        liveLink: "https://trymethod.co/",
+
+    },
+    {
+        id: 5,
         title: "Planty'x",
         description: "A landing page for a plant shop built with react and tailwind CSS",
         category: "javascript",
@@ -122,7 +131,7 @@ const Work = () => {
             </div>
             <div className="sm:w-4/5">
                 <p className='tracking-wider text-gray-light font-PPNeueMontreal p-6 md:w-fit  text-sm max-w-prose leading-8 lg:text-center'>
-                    My <a className='text-mint font-bold' href="https://drive.google.com/file/d/1ILB6oqooWTy4HROcMPL8rdp9QzLVpOEx/view?usp=drivesdk" target='_blank' >resume</a> and other projects are available on <a className='text-mint font-bold' href="https://github.com/Rei-ne" target='_blank' >github</a>.
+                    My <a className='text-mint font-bold' href="https://drive.google.com/file/d/1ILB6oqooWTy4HROcMPL8rdp9QzLVpOEx/view?usp=drivesdk" target='_blank'>resume</a> and other projects are available on <a className='text-mint font-bold' href="https://github.com/Rei-ne" target='_blank' >github</a>.
                     Follow me on  <a className='text-mint font-bold' href="https://medium.com/@reinetoyosii" target='_blank'>medium</a> and <a className='text-mint font-bold' href="https://reine.hashnode.dev/" target='_blank' >hashnode</a> where I write about my experiences.
                 </p>
             </div>
