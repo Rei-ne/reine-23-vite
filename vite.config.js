8437942ea71b930568d4857813d8df497e211b9b
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2026-08-11 20:50:33 +0100

    chore: add Sites deployment output

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), NodeGlobalsPolyfillPlugin({
-    buffer: true
-  })],
+  plugins: [react()],
+  build: {
+    outDir: 'dist/client',
+  },
 })
