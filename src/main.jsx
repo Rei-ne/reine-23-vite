ccc8d3c14e118f57a38d0d90c6adfb5c2fb58317
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2026-08-11 20:50:33 +0100

    fix: restore GA4 page tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ReactGA from 'react-ga4'
+
+const GA_MEASUREMENT_ID = 'G-K7W9HJD4TW'
+
+ReactGA.initialize(GA_MEASUREMENT_ID)
+ReactGA.send({
+  hitType: 'pageview',
+  page: window.location.pathname + window.location.search,
+  title: document.title,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
